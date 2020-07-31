392b73e56b6681702e5e12ea4d3f56447b4117a2
hudsonsr <hudsonsr@gmail.com>
2020-07-31 12:35:51 -0300

    initial comit

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,90 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-   return <h1>teste</h1>;
+   return (
+      <ProductList>
+         <li>
+            <img
+               src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-creation-21-masculino/42/D16-5429-042/D16-5429-042_zoom2.jpg?ts=1588782638&ims=326x"
+               alt="Tenis"
+            />
+            <strong>Nome do produto</strong>
+            <span>R$ 10,50</span>
+
+            <button type="button">
+               <div>
+                  <MdAddShoppingCart size={16} color="#fff" />3
+               </div>{' '}
+               <span>ADICIONAR AO CARRINHO</span>
+            </button>
+         </li>
+
+         <li>
+            <img
+               src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-creation-21-masculino/42/D16-5429-042/D16-5429-042_zoom2.jpg?ts=1588782638&ims=326x"
+               alt="Tenis"
+            />
+            <strong>Nome do produto</strong>
+            <span>R$ 10,50</span>
+
+            <button type="button">
+               <div>
+                  <MdAddShoppingCart size={16} color="#fff" />3
+               </div>{' '}
+               <span>ADICIONAR AO CARRINHO</span>
+            </button>
+         </li>
+
+         <li>
+            <img
+               src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-creation-21-masculino/42/D16-5429-042/D16-5429-042_zoom2.jpg?ts=1588782638&ims=326x"
+               alt="Tenis"
+            />
+            <strong>Nome do produto</strong>
+            <span>R$ 10,50</span>
+
+            <button type="button">
+               <div>
+                  <MdAddShoppingCart size={16} color="#fff" />3
+               </div>{' '}
+               <span>ADICIONAR AO CARRINHO</span>
+            </button>
+         </li>
+
+         <li>
+            <img
+               src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-creation-21-masculino/42/D16-5429-042/D16-5429-042_zoom2.jpg?ts=1588782638&ims=326x"
+               alt="Tenis"
+            />
+            <strong>Nome do produto</strong>
+            <span>R$ 10,50</span>
+
+            <button type="button">
+               <div>
+                  <MdAddShoppingCart size={16} color="#fff" />3
+               </div>
+               <span>ADICIONAR AO CARRINHO</span>
+            </button>
+         </li>
+
+         <li>
+            <img
+               src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-creation-21-masculino/42/D16-5429-042/D16-5429-042_zoom2.jpg?ts=1588782638&ims=326x"
+               alt="Tenis"
+            />
+            <strong>Nome do produto</strong>
+            <span>R$ 10,50</span>
+
+            <button type="button">
+               <div>
+                  <MdAddShoppingCart size={16} color="#fff" />3
+               </div>{' '}
+               <span>ADICIONAR AO CARRINHO</span>
+            </button>
+         </li>
+      </ProductList>
+   );
 }
